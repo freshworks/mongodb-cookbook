93d34c418cc7b484164b5a84bddadfd902e0a295
Balaji29 <balaji.rk@freshdesk.com>
2016-11-14 22:29:13 +0530

    configpath removed from mongos

--- definitions/mongodb.rb
+++ definitions/mongodb.rb
@@ -132,7 +132,7 @@ define :mongodb_instance,
     provider = "mongos"
     # mongos will fail to start if dbpath is set
     node[:mongodb][:config].delete('dbpath')
-    node[:mongodb][:config][:configdb] = new_resource.configserver_nodes.collect{|n| "#{(n['mongodb']['configserver_url'] || n['fqdn'])}:#{n['mongodb']['config']['port']}" }.sort.join(",")
+    # node[:mongodb][:config][:configdb] = new_resource.configserver_nodes.collect{|n| "#{(n['mongodb']['configserver_url'] || n['fqdn'])}:#{n['mongodb']['config']['port']}" }.sort.join(",")
   end
   
   # default file

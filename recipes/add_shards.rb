30baae4b487067a5d9c3c3875e7ea82d089face6
Shreyas <shreyas.ns@freshdesk.com>
2016-11-21 02:51:20 +0530

    fix add_shard recipe
    Signed-off-by: Shreyas <shreyas.ns@freshdesk.com>

--- recipes/add_shards.rb
+++ recipes/add_shards.rb
@@ -1,6 +1,6 @@
 shard_nodes = search(
   :node,
-  "mongodb_cluster_name:#{new_resource.cluster_name} AND \
+  "mongodb_cluster_name:#{node['mongodb']['cluster_name']} AND \
    mongodb_is_shard:true AND \
    chef_environment:#{node.chef_environment}"
 )

f9d8f65dfa0feca214b1388378b5c88efc31daf0
Shreyas <shreyas.ns@freshdesk.com>
2016-11-21 01:30:45 +0530

    back to 1.12.5
    Signed-off-by: Shreyas <shreyas.ns@freshdesk.com>

--- attributes/default.rb
+++ attributes/default.rb
@@ -110,7 +110,7 @@ default[:mongodb][:key_file_content] = nil
 # install the mongo and bson_ext ruby gems at compile time to make them globally available
 # TODO: remove bson_ext once mongo gem supports bson >= 2
 default['mongodb']['ruby_gems'] = {
-  :mongo => '2.3.1',
-  :bson => '2.3.1',
+  :mongo => '1.12.5',
+  :bson_ext => '1.12.5',
   
 }

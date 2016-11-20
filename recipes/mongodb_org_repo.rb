c5f144c7d07acd1b8ff8177135ee29b06cb66669
Shreyas <shreyas.ns@freshdesk.com>
2016-11-20 15:57:19 +0530

    fix yum repo
    Signed-off-by: Shreyas <shreyas.ns@freshdesk.com>

--- recipes/mongodb_org_repo.rb
+++ recipes/mongodb_org_repo.rb
@@ -36,7 +36,7 @@ when 'debian'
   end
 
 when 'rhel', 'fedora'
-  yum_repository 'mongodb' do
+  yum_repository 'mongodb-org' do
     description 'mongodb RPM Repository'
     baseurl "#{node['mongodb']['repo']}/#{node[:mongodb][:package_base_version]}/#{node['kernel']['machine']  =~ /x86_64/ ? 'x86_64' : 'i686'}"
     action :create
